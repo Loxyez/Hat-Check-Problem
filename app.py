25c5eef3cbef4f27c6c6036c5d5a657a27fed97a
Chanatip Deemee <78657820+Loxyez@users.noreply.github.com>
2023-10-13 22:27:22 +0700

    First Draft

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# variable that must be natural number
+def derangements(n: int, memo={0:1, 1: 0, 2: 1}):
+
+    # Incase of n = 1 or n = 2 it just an n - 1
+    if n in memo:
+        return memo[n]
+    
+    # recursive the recurrence relation
+    # return the result of !(n-1) * d(n - 1) + d(n - 2)
+    return (n - 1) * (derangements(n - 1) + derangements(n - 2))
+
+MAX_LIMIT = 45
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    error = None
+    if request.method == 'POST':
+        try:
+            n = int(request.form.get('n'))
+            if n > MAX_LIMIT or n <= 0:
+                error = f"You are using the maximum recursion depth exceeded Please enter a value less than {MAX_LIMIT} and more or equal to 0."
+            else:
+                result = derangements(n)
+        except ValueError:
+            result = "Invalid input. Please enter a positive integer."
+
+    return render_template('index.html', result=result, error=error)
+
+if __name__ == '__main__':
+    app.run(debug=True)
